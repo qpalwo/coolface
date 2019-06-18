09d1a8d74158ecfba8081fb97229878955449bb7
Rimson Liu <rimson@qq.com>
2019-06-18 16:44:35 +0800

    Add Login and SignUp

--- app/src/main/java/com/hustunique/coolface/bean/User.kt
+++ app/src/main/java/com/hustunique/coolface/bean/User.kt
@@ -1,6 +1,8 @@
 package com.hustunique.coolface.bean
 
+import cn.bmob.v3.BmobUser
+
 /**
  * 用户
  */
-data class User(var username: String, var phone: String, var posts: List<Post>)
+data class User(var nickname: String, var posts: List<Post>) : BmobUser()
