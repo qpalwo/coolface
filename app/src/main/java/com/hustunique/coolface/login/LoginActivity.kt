2d62214b4e79b198ccca2502381dfeda38c8a427
Rimson Liu <rimson@qq.com>
2019-06-18 11:25:33 +0800

    添加Bmob依赖

---
+++ app/src/main/java/com/hustunique/coolface/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.hustunique.coolface.login
+
+import android.os.Bundle
+import com.hustunique.coolface.R
+import com.hustunique.coolface.base.BaseActivity
+
+class LoginActivity : BaseActivity(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
